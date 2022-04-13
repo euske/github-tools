276821d5bd0900edb211d1bc74066ad6e45e4479
Yusuke Shinyama <yusuke@shinyama.jp>
2022-04-13 15:33:10 +0900

    Oops.

--- unpack_repo.py
+++ unpack_repo.py
@@ -76,7 +76,7 @@ def unpack_repo(zippath, dstbase, pats=[], extract=False,
                 logging.error(f'error: {zippath}/{src!r}: {e}')
             except (zipfile.BadZipFile, zipfile.zlib.error) as e:
                 logging.error(f'error: {zippath}/{src!r}: {e}')
-        logging.info(f'extracted: {zippath}: {extracted} files.')
+    logging.info(f'extracted: {zippath}: {extracted} files.')
     return
 
 def main(argv):
